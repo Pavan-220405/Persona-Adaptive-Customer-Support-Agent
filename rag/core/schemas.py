c33dc9d58c8dd1b0708a9ef9444277f227dcc9a9
Pavan G <pavan220405@gmail.com>
2026-03-05 01:02:27 +0530

    Remove duplicate rag folder

--- rag/core/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import Annotated, Literal
-from langchain_core.messages import BaseMessage
-from langgraph.graph.message import add_messages
-
-
-class persona_classification(BaseModel):
-    customer_persona : Annotated[Literal["technical_expert", "frustrated_user", "business_executive"],Field(...,description="Identify the persona of the user")]
-    score : Annotated[int,Field(...,description="Score of the detected persona")]
